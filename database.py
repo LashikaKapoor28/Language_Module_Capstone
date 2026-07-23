4003c25c016c4e8e255285ca9bdabe4e4efd5720
NihanthT <tatikonda.nihanth@gmail.com>
2026-07-22 23:26:46 -0700

    basic database stubs

---
+++ database.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+class ImageDatabase:
+      def __init__(self):
+            pass
+
+      def descriptor_to_embedding(descriptor):
+            pass
+
+      def query():
+            pass
+
+      def display_images(urls):
+            st.image(urls, width=300)
